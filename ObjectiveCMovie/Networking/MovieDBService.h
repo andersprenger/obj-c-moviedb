6486f940ed4141c458dd9b33f703c4346d5fed85
Giovani Bettoni <bettoni.gn@gmail.com>
2022-03-21 15:25:54 -0300

    Initial commit

---
+++ ObjectiveCMovie/Networking/MovieDBService.h
@@ -0,0 +1,12 @@
+//
+//  MovieDBService.h
+//  ObjectiveCMovie
+//
+//  Created by Giovani Nícolas Bettoni on 21/03/22.
+//
+
+#ifndef MovieDBService_h
+#define MovieDBService_h
+
+
+#endif /* MovieDBService_h */
---
+++ ObjectiveCMovie/Networking/MovieDBService.m
@@ -0,0 +1,8 @@
+//
+//  MovieDBService.m
+//  ObjectiveCMovie
+//
+//  Created by Giovani Nícolas Bettoni on 21/03/22.
+//
+
+#import <Foundation/Foundation.h>
